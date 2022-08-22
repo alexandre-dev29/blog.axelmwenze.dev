2756439aec65fcec90ad08028a75df06e7c2a173
alexandre-dev29 <axel.business29@gmail.com>
2022-08-22 18:34:19 +0200

    Updating the category for fetching data from server

--- libs/ui/src/nav-bar/nav-bar.tsx
+++ libs/ui/src/nav-bar/nav-bar.tsx
@@ -3,51 +3,71 @@ import { Dropdown } from '@nextui-org/react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { useGetAllCategoriesQuery } from '@next-template-nx/data';
+import { defaultApolloClient } from '@next-template-nx/utils';
 
 type MenuItem = { name: string; link: string; key: string };
-const listLinks: Array<MenuItem> = [
-  { link: '/', name: 'Popular', key: 'popular' },
-  { link: '/about', name: 'Engineering', key: 'engineering' },
-  { link: '/work', name: 'Growth Marketing', key: 'growth-marketing' },
-  { link: '/skills', name: 'Company', key: 'company' },
-];
 
 export function NavBar() {
   const [isSSR, setIsSSR] = useState(true);
+  const [listOfMenu, setListOfMenu] = useState([] as Array<MenuItem>);
+  const { loading, data } = useGetAllCategoriesQuery({
+    errorPolicy: 'all',
+    fetchPolicy: 'cache-first',
+    client: defaultApolloClient,
+  });
   useEffect(() => {
     setIsSSR(false);
-  }, []);
+    if (!loading) {
+      const finalValue = [] as Array<MenuItem>;
+      data?.categories?.data.forEach((value) => {
+        finalValue.push({
+          name: `${value.attributes?.CategoryName}`,
+          key: `${value.attributes?.CategoryName.toLowerCase()}`,
+          link: `${value.attributes?.CategoryName}`,
+        });
+      });
+      setListOfMenu(finalValue);
+    }
+  }, [data?.categories?.data, loading]);
+
+  const selectedStyle = 'border-b-2 border-[#313bac] text-secondary';
+
   return (
     <div
       className={
         'px-10 sm:px-24 md:px-28 lg:px-36 xl:px-48 2xl:px-56 flex justify-between items-center'
       }
     >
-      <h1 className={'text-lg lg:text-3xl font-extrabold default-police '}>
-        Axel <span className={'text-secondary'}>Mwenze</span>
-      </h1>
+      <Link href={'/'}>
+        <h1
+          className={
+            'text-lg lg:text-3xl font-extrabold default-police cursor-pointer'
+          }
+        >
+          Axel <span className={'text-secondary'}>Mwenze</span>
+        </h1>
+      </Link>
       <div className={'flex flex-1 hidden md:block'}>
         <div className={'flex-1 flex gap-8 justify-end'}>
-          <button
-            className={
-              'py-4 text-sm lg:text-lg border-b-2 border-[#313bac] text-secondary font-bold '
-            }
-          >
-            Popular
-          </button>
-          <button className={'py-4 text-sm lg:text-lg font-bold text-gray-600'}>
-            Engineering{' '}
-          </button>
-          <button
-            className={'py-4 text-sm lg:text-lg  font-bold text-gray-600'}
-          >
-            Growth Marketing{' '}
-          </button>
-          <button
-            className={'py-4 text-sm lg:text-lg  font-bold text-gray-600'}
-          >
-            Company
-          </button>
+          {loading ? (
+            <p>Loading.....</p>
+          ) : (
+            data?.categories?.data.map((value) => (
+              <Link
+                href={`/category/${value.attributes?.CategoryName}`}
+                key={value.id}
+              >
+                <button
+                  className={
+                    'py-4 text-sm lg:text-lg  text-gray-600 font-bold '
+                  }
+                >
+                  {value.attributes?.CategoryName}
+                </button>
+              </Link>
+            ))
+          )}
         </div>
       </div>
       <div className={'md:hidden '}>
@@ -56,10 +76,10 @@ export function NavBar() {
             <Dropdown.Button flat>
               <AiOutlineMenu className={'text-xl'} />
             </Dropdown.Button>
-            <Dropdown.Menu aria-label="Dynamic Actions" items={listLinks}>
+            <Dropdown.Menu aria-label="Dynamic Actions" items={listOfMenu}>
               {(item: any) => (
                 <Dropdown.Item key={item.key} color={'secondary'}>
-                  <Link href={`${item.link}`}>
+                  <Link href={`/category/${item.link}`}>
                     <p className={'font-bold default-police'}>{item.name}</p>
                   </Link>
                 </Dropdown.Item>
